69eebe6d427f21abf8e08c2ea37ab0e21d321492
Rishad <59338957+micr0chip@users.noreply.github.com>
2022-05-20 23:42:47 +0700

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="footer-content">
+          <div class="logo-home"></div>
+          <div class="footer-text">
+            <div class="social-links">
+              <span class="social github"><a href="https://github.com/micr0chip" target="_blank">Github</a></span>
+            </div>
+
+            <p>© 2022 MarvelComic's. Все права защищены.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  min-height: 300px;
+  text-align: center;
+  background-color: #222;
+  padding-top: 120px;
+  font-size: 16px;
+  font-weight: 200;
+  color: #969696;
+  padding-right: 20px;
+  padding-bottom: 20px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
